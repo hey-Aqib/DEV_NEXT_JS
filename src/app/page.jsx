53aafebef70e8f6dd4df78b30d26d269d186a7d6
hey-Aqib <phatomdevel@gmail.com>
2025-03-28 15:57:28 +0500

    CPN_01 || Section5,9

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import PandaScroll from "@/components/PangaScroll";
+import Section4 from "@/components/Section4";
+
+export default function Home() {
+  return (
+   <>
+<Section4/>
+<PandaScroll/>
+   </>
+  );
+}

58925aa61ea83a72602d2064b2f0cc90d086c280
hey-Aqib <phatomdevel@gmail.com>
2025-04-15 22:47:43 +0500

    3D

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
 
-import Section6 from "@/Dev/components/Section6";
 import Hero from "@/Dev/Components/Hero";
 import Navbar from "@/Dev/Components/Navbar";
 import ScrollMerge3D from "@/Dev/Components/ScrollMerge3D";
 import Section2 from "@/Dev/Components/Section2";
 import Section3 from "@/Dev/Components/Section3";
+import Section6 from "@/Dev/Components/Section6";
 import Section5 from "@/Dev/Components/Section5";
 import Section7 from "@/Dev/Components/Section7";
 import Section8 from "@/Dev/Components/Section8";
@@ -28,7 +28,7 @@ export default async function Home() {
     <Hero/>
     <Section2/>
     <Section3/>
-    {/* <ScrollMerge3D/> */}
+    <ScrollMerge3D/>
     <Section5/>
     <Section6/>
     <Section7/>

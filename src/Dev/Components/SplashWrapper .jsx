ea96cbecd125ac7cc0a61d5898387ebb80b79801
hey-Aqib <Phantomdevel@gmail.com>
2025-05-03 20:23:44 +0500

    remove loader

--- src/Dev/Components/SplashWrapper .jsx
+++ src/Dev/Components/SplashWrapper .jsx
@@ -1,12 +1,9 @@
 'use client';
-import { useState, useEffect, lazy, Suspense } from 'react';
+import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation'; 
 import LoadingScreen from './LoadingScreen';
-import Loader from './ui/Loader';
-
-const Header = lazy(() => import('./navbar/Header'));
-const Footer = lazy(() => import('./Footer'));
-
+import Footer from './Footer';
+import Header from './navbar/Header';
 
 export default function SplashWrapper({ children }) {
   const [loading, setLoading] = useState(true);
@@ -23,17 +20,21 @@ export default function SplashWrapper({ children }) {
   return (
     <>
       {loading && (
-        <div className="fixed inset-0 z-10 flex items-center justify-center bg-black text-white transition-opacity duration-1000 opacity-100">
+        <div
+          className="fixed inset-0 z-10 flex items-center justify-center bg-black text-white transition-opacity duration-1000 opacity-100"
+        >
           <LoadingScreen />
         </div>
       )}
 
-      <div className={`transition-opacity duration-1000 ${loading ? 'opacity-0' : 'opacity-100'}`}>
-        <Suspense fallback={<Loader />}>
-          <Header />
-          {!loading && children}
-          <Footer key={pathname}/> 
-        </Suspense>
+      <div
+        className={`transition-opacity duration-1000 ${
+          loading ? 'opacity-0' : 'opacity-100'
+        }`}
+      >
+        <Header />
+        {children}
+        <Footer key={pathname}/> 
       </div>
     </>
   );

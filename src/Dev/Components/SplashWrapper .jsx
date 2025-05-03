a578bfb3e2f0bfed53e766041e4dc7e29e16110c
hey-Aqib <Phantomdevel@gmail.com>
2025-05-03 23:38:55 +0500

    Dev__

--- src/Dev/Components/SplashWrapper .jsx
+++ src/Dev/Components/SplashWrapper .jsx
@@ -19,19 +19,17 @@ export default function SplashWrapper({ children }) {
 
   return (
     <>
-      {/* Loading Screen with transition only for fading out */}
-      {loading && (
-        <div className={`fixed inset-0 z-10 flex items-center justify-center bg-black text-white transition-opacity duration-1000 ${loading ? 'opacity-100' : 'opacity-0'}`}>
+      {loading ? (
+        <div className="fixed inset-0 z-10 flex items-center justify-center bg-black text-white transition-opacity duration-1000 opacity-100">
           <LoadingScreen />
         </div>
-      )}
-
-      {/* Main content with transition opacity based on loading state */}
-      <div className={`transition-opacity duration-1000 ${loading ? 'opacity-0' : 'opacity-100'}`}>
+      ) : (
+        <div className="transition-opacity duration-1000 opacity-100">
           <Header />
-          {!loading && children}
+          {children}
           <Footer key={pathname} />
-      </div>
+        </div>
+      )}
     </>
   );
 }
